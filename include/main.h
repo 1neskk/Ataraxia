6d348db550507d636b16a3cea5d490f6bb757b22
1neskk <contato.nesk@outlook.com>
2024-05-22 02:24:53 -0300

    Remade three dimensional vector data type

--- include/main.h
+++ include/main.h
@@ -9,31 +9,68 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../thirdparty/stb/stb_image_write.h"
 
-struct Vec3
+template <class T>
+class vec3
 {
-	float x, y, z;
-	__host__ __device__ Vec3() : x(0.0f), y(0.0f), z(0.0f) {}
-	__host__ __device__ Vec3(const float x, const float y, const float z) : x(x), y(y), z(z) {}
-
-    __host__ __device__ Vec3 operator+(const Vec3& v) const { return { x + v.x, y + v.y, z + v.z }; }
-    __host__ __device__ Vec3 operator-(const Vec3& v) const { return { x - v.x, y - v.y, z - v.z }; }
-    __host__ __device__ Vec3 operator*(const float s) const { return { x * s, y * s, z * s }; }
-    __host__ __device__ Vec3 operator/(const float s) const { return { x / s, y / s, z / s }; }
-
-    // scalar
-    __host__ __device__ Vec3 operator*(const Vec3& v) const { return { x * v.x, y * v.y, z * v.z }; }
-    __host__ __device__ Vec3 operator/(const Vec3& v) const { return { x / v.x, y / v.y, z / v.z }; }
-
-	__host__ __device__ float length() const { return sqrtf(x * x + y * y + z * z); }
-	__host__ __device__ Vec3 normalize() const { return *this / length(); }
-	__host__ __device__ static float dot(const Vec3& a, const Vec3& b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
-	
-	__host__ __device__ static Vec3 cross(const Vec3& a, const Vec3& b)
+public:
+    union
     {
-        return { a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x };
+        struct { T x, y, z; };
+        struct { T r, g, b; };
+        struct { T V[3]; };
+    };
+
+    __host__ __device__ vec3() : x(0), y(0), z(0) {}
+    __host__ __device__ vec3(T x, T y, T z) : x(x), y(y), z(z) {}
+    __host__ __device__ vec3(const vec3& v) : x(v.x), y(v.y), z(v.z) {}
+
+    // operators
+    __host__ __device__ vec3& operator=(const vec3& v) { x = v.x; y = v.y; z = v.z; return *this; }
+
+    __host__ __device__ vec3& operator+=(const vec3& v) { x += v.x; y += v.y; z += v.z; return *this; }
+    __host__ __device__ vec3& operator-=(const vec3& v) { x -= v.x; y -= v.y; z -= v.z; return *this; }
+    __host__ __device__ vec3& operator*=(T s) { x *= s; y *= s; z *= s; return *this; }
+    __host__ __device__ vec3& operator/=(T s) { x /= s; y /= s; z /= s; return *this; }
+
+    __host__ __device__ vec3& operator+(const vec3& v) const { return vec3(x + v.x, y + v.y, z + v.z); }
+    __host__ __device__ vec3& operator-(const vec3& v) const { return vec3(x - v.x, y - v.y, z - v.z); }
+    __host__ __device__ vec3& operator*(T s) const { return vec3(x * s, y * s, z * s); }
+    __host__ __device__ vec3& operator/(T s) const { return vec3(x / s, y / s, z / s); }
+
+    __host__ __device__ friend vec3 operator*(T s, const vec3& v) { return vec3(s * v.x, s * v.y, s * v.z); }
+    __host__ __device__ friend vec3 operator/(T s, const vec3& v) { return vec3(s / v.x, s / v.y, s / v.z); }
+
+    __host__ __device__ vec3& operator++() { x++; y++; z++; return *this; }
+    __host__ __device__ vec3& operator--() { x--; y--; z--; return *this; }
+    __host__ __device__ vec3 operator++(int) { vec3 v(*this); x++; y++; z++; return v; }
+    __host__ __device__ vec3 operator--(int) { vec3 v(*this); x--; y--; z--; return v; }
+
+    __host__ __device__ vec3 operator-() const { return vec3(-x, -y, -z); }
+
+    bool operator==(const vec3& v) const { return x == v.x && y == v.y && z == v.z; }
+    bool operator!=(const vec3& v) const { return x != v.x || y != v.y || z != v.z; }
+
+    T dot(const vec3& v) const { return x * v.x + y * v.y + z * v.z; }
+    vec3 cross(const vec3& v) const { return Vec3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
+    T length() const { return sqrt(x * x + y * y + z * z); }
+    vec3 normalize() const { return *this / length(); }
+
+    friend std::ostream& operator<<(std::ostream& os, const vec3& v)
+    {
+        os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+        return os;
     }
 
+    friend std::istream& operator>>(std::istream& is, vec3& v)
+    {
+        is >> v.x >> v.y >> v.z;
+        return is;
+    }
+
+    T& operator[](int i) { return V[i]; }
+    const T& operator[](int i) const { return V[i]; }
 };
+typedef vec3<float> Vec3;
 
 struct Ray
 {
